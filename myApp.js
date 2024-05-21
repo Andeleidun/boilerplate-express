fcf08363e0cc501b2cd663f020ee950298d4ce8c
Adair Daniels <adairdaniels@gmail.com>
2024-05-21 16:46:58 -0700

    through lesson 6

--- myApp.js
+++ myApp.js
@@ -1,19 +1,22 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
+app.get('/', (req, res) => {
+    const path = __dirname + '/views/index.html';
+    res.sendFile(path);
+});
 
+app.use('/public', express.static(__dirname + '/public'));
 
-
-
-
-
-
-
-
-
-
-
-
+app.get('/json', (req, res) => {
+    const response = {
+        "message": "Hello json"
+    };
+    const messageStyle = process.env.MESSAGE_STYLE;
+    if (messageStyle === 'uppercase') response.message = response.message.toLocaleUpperCase();
+    res.json(response);
+});
 
 
 
